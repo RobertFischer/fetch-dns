f73c0de4b76e6247f8d3000c235dff503f53e88c
Robert Fischer <robert.fischer@k4connect.com>
2020-09-04 13:50:47 -0400

    build(generate-rrtypes): allow the script for generating rrtypes to run even without tmpdir variable

--- script/generateRrtypes.sh
+++ script/generateRrtypes.sh
@@ -6,7 +6,7 @@ set -exu
 cd "$(dirname "$0")/.."
 ROOTDIR="$(pwd)"
 
-cd "$TMPDIR"
+cd "${TMPDIR:=/tmp}"
 wget 'https://www.iana.org/assignments/dns-parameters/dns-parameters-4.csv' -O dns-rrtypes.csv
 npx csvtojson dns-rrtypes.csv | tee "$ROOTDIR/src/Rrtypes.json"
 
